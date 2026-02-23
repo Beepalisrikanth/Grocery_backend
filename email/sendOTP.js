422367ab323b25deac13d5a41247993e8ff70d90
beepali srikanth <beepalisrikanth@gamil.com>
2026-02-23 19:15:42 +0530

    updated port number in sendOtp

--- email/sendOTP.js
+++ email/sendOTP.js
@@ -6,7 +6,7 @@ dotenv.config()
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: false, // Use true for port 465, false for port 587
+    secure: true, // Use true for port 465, false for port 587
     family:4,
     auth: {
         user: process.env.EMAIL_USER,
